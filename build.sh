f7a1697f78cbf06948b162c24c905ed2a5ed2d9c
zhangweizhwe <zhangweizhwe@didichuxing.com>
2019-01-07 14:09:43 +0800

    init server

---
+++ build.sh
@@ -0,0 +1,1 @@
+go build -o test test.go
---
+++ test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello World")
+	})
+	router.Run(":8000")
+	fmt.Println("vim-go")
+}
